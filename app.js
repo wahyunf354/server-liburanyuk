6db3ab9a5d9d90570be16b3c0802c59ddf8145ed
wahyunf354 <wahyunurfadillah313@gmail.com>
2020-12-25 08:25:40 +0700

    deploy heroku

--- app.js
+++ app.js
@@ -35,10 +35,12 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(cors({
-  origin: "https://liburanyuk.vercel.app",
-  methods: ['GET', 'PUT', 'POST']
-}));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "PUT", "POST"],
+  })
+);
 app;
 app.use(methodOverride("_method"));
 app.use(flash());
